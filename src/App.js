2856feb4e4ed918799fe5c637e720bd71eb2afe7
Eduardo Pineda <edibozu@gmail.com>
2020-10-30 16:30:25 -0400

    created table and added it to app with hardcoded values, for testing purposes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+import Table from "./components/Table/Table"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <Table />
     </div>
   );
 }
